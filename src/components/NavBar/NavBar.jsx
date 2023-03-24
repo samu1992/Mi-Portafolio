38ef60539d3b4892a56e812c726ce35572b95e77
samuel carrizo <samuelcarrizot@gmail.com>
2023-03-24 18:25:42 -0300

    se agrego enlace al codigo

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { useEffect, useState } from 'react';
-/* import samuelCarrizo from '../../imagenes/samuelCarrizo.pdf'; */
+import foto from '../../imagenes/miFoto.jpg';
 import { Link } from 'react-router-dom';
 
 const NavBar = () => {
@@ -33,6 +33,7 @@ const NavBar = () => {
         <div><a target="_blank" rel='noreferrer' href="https://www.linkedin.com/in/samuel-carrizo-844980176/"><i className=" fab fa-linkedin"></i></a></div>
         <div><a target="_blank" rel='noreferrer' href="."><i className=" fab fa-instagram"></i></a></div>
         <div><a target="_blank" rel='noreferrer' href="https://github.com/samu1992"><i className="fab fa-github"></i></a></div>
+        <div><a target="_blank" rel='noreferrer' href="https://github.com/samu1992/Mi-Portafolio"><i class="fa fa-code"></i></a></div>
       </div>
       <nav className="navbar--Links">
         <ul>
@@ -48,7 +49,7 @@ const NavBar = () => {
       </button>
       <nav className={`navbar--Responsive ${menuOpen ? 'navbar--Open' : 'navbar--Closed'}`}>
         <section className='navbar--Avatar'>
-          <div className='avatar'><img alt='error al cargar mi foto' src='../../imagenes/miFoto.jpg' /></div>
+          <div className='avatar'><img alt='error al cargar mi foto' src={foto} /></div>
           <h2>Samuel Carrizo</h2>
         </section>
         <ul>
--- src/components/Presentacion/Presentacion.jsx
+++ src/components/Presentacion/Presentacion.jsx
@@ -1,18 +1,15 @@
-
-import miFoto from '../../imagenes/miFoto.jpg'
-
-
+import foto from '../../imagenes/miFoto.jpg';
 const Presentacion = () => {
-    
-return (
-    <aside className='container--Presentacion'>
-        <section className='presentacion--Parrafo'>
-            <p className='tracking-in-expand'>Hola, mi nombre es <strong>Samuel Carrizo,</strong> desarrollador web front-end creativo, apasionado por crear experiencias únicas.
-            </p>
-        </section>
-        <div className='mi--Foto'><img alt='error al cargar mi foto' src={miFoto}/></div>
-    </aside>
-)
+
+    return (
+        <aside className='container--Presentacion'>
+            <section className='presentacion--Parrafo'>
+                <p className='tracking-in-expand'>Hola, mi nombre es <strong>Samuel Carrizo,</strong> desarrollador web front-end creativo, apasionado por crear experiencias únicas.
+                </p>
+            </section>
+            <div className='mi--Foto'><img alt='error al cargar mi foto'src={foto}/></div>
+        </aside>
+    )
 }
 
 export default Presentacion
