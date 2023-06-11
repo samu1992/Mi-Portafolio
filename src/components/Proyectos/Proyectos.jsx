254f4c8b661faab79c0da5dc3a9704d981d0a9cc
samuel carrizo <samuelcarrizot@gmail.com>
2023-06-11 20:00:09 -0300

    se cambiaron algunos estilos de las card

--- src/components/Proyectos/Proyectos.jsx
+++ src/components/Proyectos/Proyectos.jsx
@@ -2,9 +2,10 @@ import elegant from '../../imagenes/elegant.png';
 import faunaUrbana from '../../imagenes/fondoGit.png';
 import portfolio from '../../imagenes/soley.png';
 import gallery from '../../imagenes/dog-gallery.png';
-import tip from '../../imagenes/tip_calculator.png';
+//import tip from '../../imagenes/tip_calculator.png';
 import Proyecto from '../Proyecto/Proyecto';
-import todo from '../../imagenes/ToDo.png';
+import todo from '../../imagenes/Todolist.png';
+import ai from '../../imagenes/TheAI.png';
 
 
 const Proyectos = () => {
@@ -19,7 +20,7 @@ const Proyectos = () => {
                 <article>
                     <Proyecto
                         title="Elegant"
-                        description="E-commerce: Desarrollo de tienda online, con una base de datos creada en firebase"
+                        description="E-commerce: Desarrollo de tienda online, con una base de datos creada en firebase. Proyecto final del curso de React js."
                         img={elegant}
                         site='https://shop-elegant.vercel.app/'
                         code='https://github.com/samu1992/Shop-Elegant' 
@@ -28,15 +29,14 @@ const Proyectos = () => {
                     <Proyecto
                         title="Fauna Urbana"
                         description="Organización benéfica
-            dedicada al rescate de perros en situación de calle y abandono."
+            dedicada al rescate de perros en situación de calle y abandono. Dicha Website se donara a la organizacion."
                         img={faunaUrbana}
                         site='https://fauna-urbana.vercel.app/'
                         code='https://github.com/samu1992/fauna-urbana'
                         tech='#React #Bootstrap #JavaScript #Vite' />
                     <Proyecto
-                        title="Proyecto en Equipo"
-                        description="Desarrollo de proyecto freelance utilizando React JS,
-            Bootstrap y React Router DOM."
+                        title="Portafolio"
+                        description="Desarrollo de proyecto freelance, se trata de un portafolio hecho para un cliente real, el mismo se encuentra en uso. "
                         img={portfolio}
                         site='https://soley-porfolio.vercel.app/'
                         code='https://github.com/samu1992/soley-porfolio'
@@ -50,6 +50,14 @@ const Proyectos = () => {
                         tech='#React Next js #Tailwind'
                         />
                         <Proyecto
+                        title='The AI Helper'
+                        description='Proyecto en desarrollo, es una Landing Page constantemente estoy subiendo cambios, dentro de poco el mismo estara mas completo.'
+                        img={ai}
+                        site='https://the-ai-helper.vercel.app/'
+                        code='https://github.com/samu1992/the-ai-helper'
+                        tech='#React #Vite #Sass #JavaScript'
+                        />
+                        <Proyecto
                         title="To Do List"
                         description="Lista de tareas: En proceso de desarrollo, con la finalidad de mostrar habilidades en manejo de datos, en este caso una lista de tareas."
                         img={todo}
@@ -57,14 +65,14 @@ const Proyectos = () => {
                         code="https://github.com/samu1992/to-do"
                         tech='#React #Vite #JavaScript'
                         />
-                        <Proyecto
+                        {/* <Proyecto
                         title="Tip Calculator"
                         description="Esta es una calculadora de propina simple hecha con React y Vite. Fue creada con fines educativos como parte de la práctica de desarrollo web."
                         img={tip}
                         site="https://tip-calculator-eosin.vercel.app/"
                         code="https://github.com/samu1992/tip-calculator"
                         tech="#React #Vite #JavaScript"
-                        />
+                        /> */}
                 </article>
             </aside>
         </main>
